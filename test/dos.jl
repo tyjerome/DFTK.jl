666ebd661eaf5db182f9f0a63c66e712b02c98f8
xquan818 <840169780@qq.com>
2024-04-02 19:18:01 +0200

    dos_test

--- test/dos.jl
+++ test/dos.jl
@@ -19,7 +19,7 @@ end
 
     atoms=fill(ElementPsp(testcase.atnum; psp=load_psp(testcase.psp_upf)), 2)
 	for a in collect(1:0.2:2)
-		model = model_LDA(a * testcase.lattice, atoms, testcase.positions; temperature=1e-2)
+		model = model_LDA(a * testcase.lattice, atoms, 1.5.*testcase.positions; temperature=5e-3)
 		basis = PlaneWaveBasis(model; Ecut=12, testcase.kgrid)
 		scfres = self_consistent_field(basis, tol=1e-8);
 		plot_pdos(1,scfres)
